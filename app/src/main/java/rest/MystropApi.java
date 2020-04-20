915dfab721e8f3ce559f7893ebb027ab82d98a55
gustavosb8 <gustavo_s.bastos@hotmail.com>
2020-04-20 09:35:45 -0300

    First commit

---
+++ app/src/main/java/rest/MystropApi.java
@@ -0,0 +1,26 @@
+package rest;
+
+import java.util.List;
+
+import model.Estacao;
+import model.Linha;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface MystropApi {
+
+    @GET("estacoes?operacao=buscaPorLugar")
+    Call<List<Estacao>> getEstacoesByPlace(@Query("lugar") String lugar, @Query("latitude") double latitude, @Query("longitude") double longitude);
+
+    @GET("linhas?operacao=busca")
+    Call<Linha> getLinha(@Query("id") String id);
+
+    @GET("linhas?operacao=buscaPorEstacao")
+    Call<List<Linha>> getLinhas(@Query("estacaoOrigem")Integer id1, @Query("estacaoDestino") Integer id2);
+
+    @GET("linhas?operacao=buscaTodos")
+    Call<List<Linha>> getLinhas();
+
+}
